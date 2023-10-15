205d51847f000c60094f9e99aa5dcffef26f2b1c
Tedros <tedsaasfaha@gmail.com>
2023-10-15 04:05:13 -0700

    City class created in city.py file.

---
+++ models/city.py
@@ -0,0 +1,16 @@
+#!/user/bin/python3
+"""Defines the City class which inherits from BaseModel parent class."""
+
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    """Represents a city.
+
+    Attributes:
+    state_id (str): The state id.
+    name (str): The name of this city
+    """
+
+    state_id = ""
+    name = ""
