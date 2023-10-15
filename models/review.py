cf7f915c42f7a1627a8618c3d13e02b65b909be9
Tedros <tedsaasfaha@gmail.com>
2023-10-15 04:08:14 -0700

    Review class created in review.py file.

---
+++ models/review.py
@@ -0,0 +1,18 @@
+#!/user/bin/python3
+"""Defines the Review class which inherits from BaseModel parent class."""
+
+from models.base_model import BaseModel
+
+
+class Review(BaseModel):
+    """Represents the review.
+
+    Attributes:
+    place_id (str): The Place id.
+    user_id (str): The User id.
+    text (str): The text of the review.
+    """
+
+    place_id = ""
+    user_id = ""
+    text = ""
